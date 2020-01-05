99aaa494b95f9176a47cc3d9e3aff2a4b802d9b0
Thiago Cordeiro <thiagoguetten@gmail.com>
2020-01-05 19:32:52 +0100

    Update JsonSerializer.php

--- src/JsonSerializer.php
+++ src/JsonSerializer.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Serializer;
 
+use JsonException;
 use ReflectionException;
 use Serializer\Exception\ClassMustHaveAConstructor;
 use Serializer\Exception\UnableToLoadOrCreateCacheClass;
@@ -16,13 +17,13 @@ class JsonSerializer extends Serializer
      * @throws ClassMustHaveAConstructor
      * @throws UnableToLoadOrCreateCacheClass
      * @throws ReflectionException
+     * @throws JsonException
      */
     public function deserialize($data, string $class)
     {
-        return $this->deserializeData(
-            json_decode($data, false),
-            $class
-        );
+        $json = json_decode($data, false, 512, JSON_THROW_ON_ERROR);
+
+        return $this->deserializeData($json, $class);
     }
 
     /**
